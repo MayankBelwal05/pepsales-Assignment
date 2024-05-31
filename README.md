c28a51b83cad4bdc44cab4f0671d5752cf3bc0cb
MayankBelwal05 <mbelwal05@gmail.com>
2024-05-31 11:16:57 +0530

    api limited

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## Pepsales
+## Pepsales  📈 N S E
+
+## Introduction 🔔
+
+NSE Stock Tracker is a web application designed to display live stock prices of companies listed on the National Stock Exchange (NSE). It provides users with real-time data sourced from Yahoo Finance or any other reliable source. The application includes features such as details page for each stock with price details, and an optional graph displaying price movement as Market changes 24 hours. It utilizes React components and Redux for state management.
--- frontend/src/redux/cryptoSlice.js
+++ frontend/src/redux/cryptoSlice.js
@@ -28,7 +28,7 @@ export const { fetchCryptoStart, fetchCryptoSuccess, fetchCryptoFailure } = cryp
 export const fetchCryptoData = () => async (dispatch) => {
   dispatch(fetchCryptoStart());
   try {
-    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false');
     dispatch(fetchCryptoSuccess(response.data));
   } catch (error) {
     dispatch(fetchCryptoFailure(error.message));
